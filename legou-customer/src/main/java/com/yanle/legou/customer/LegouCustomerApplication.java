5178bb0ce0cf61af4b41034e4be4e2f1e91a3b63
le.yan <le.yan@dmall.com>
2018-07-18 15:26:58 +0800

    整合hystrix dashboard

--- legou-customer/src/main/java/com/yanle/legou/customer/LegouCustomerApplication.java
+++ legou-customer/src/main/java/com/yanle/legou/customer/LegouCustomerApplication.java
@@ -2,12 +2,18 @@ package com.yanle.legou.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrixDashboard
+@EnableHystrix
+@EnableCircuitBreaker
 public class LegouCustomerApplication {
 
     public static void main(String[] args) {
